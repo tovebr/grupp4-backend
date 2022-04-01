a9e863723f23df550a4b4c91b965803d3b29ca6e
jesperholmlund <jesper.holmlund@student.kyh.se>
2022-04-01 11:06:07 +0200

    update function

--- front/script.js
+++ front/script.js
@@ -15,19 +15,62 @@ const printChairs = (chairs) => {
   chairs.map((chair) => {
     const chairDiv = document.createElement("div");
     const deleteBtn = document.createElement("button");
-    const updateButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    chairDiv.classList.add("chair");
     deleteBtn.innerHTML = "delete";
-    updateButton.innerHTML = "update";
-    chairDiv.innerHTML = `<div id=${chair.id} ><h3>${chair.attributes.title}</h3><p>${chair.attributes.description}</p></div>`;
+    editButton.innerHTML = "edit";
+    chairDiv.innerHTML = `<div id=${chair.id} ><h3>${chair.attributes.title}</h3><p>Price: ${chair.attributes.price} SEK</p><p>${chair.attributes.description}</p></div>`;
     chairDisplay.appendChild(chairDiv);
     chairDisplay.appendChild(deleteBtn);
-    chairDisplay.appendChild(updateButton);
+    chairDisplay.appendChild(editButton);
     deleteBtn.addEventListener("click", async () => {
       await fetch("http://localhost:1337/api/chairs/" + chair.id, {
         method: "delete",
       });
       chairDisplay.innerHTML = "";
       fetchChair();
+    });
+    editButton.addEventListener("click", async () => {
+      editButton.disabled = true;
+      const updateChair = document.createElement("button");
+      const newTitle = document.createElement("input");
+      const newQty = document.createElement("input");
+      const newPrice = document.createElement("input");
+      const newDescription = document.createElement("textarea");
+      newTitle.value = chair.attributes.title;
+      newQty.setAttribute("type", "number");
+      newQty.value = chair.attributes.qty;
+      newPrice.setAttribute("type", "number");
+      newPrice.value = chair.attributes.price;
+      newDescription.value = chair.attributes.description;
+      newDescription.setAttribute("rows", "4");
+      updateChair.innerHTML = "Update";
+      chairDiv.appendChild(newTitle);
+      chairDiv.appendChild(newPrice);
+      chairDiv.appendChild(newQty);
+      chairDiv.appendChild(newDescription);
+      chairDiv.appendChild(updateChair);
+      updateChair.addEventListener("click", async () => {
+        console.log(newTitle.value);
+        let obj = {
+          data: {
+            title: newTitle.value,
+            price: newPrice.value,
+            qty: newQty.value,
+            description: newDescription.value,
+          },
+        };
+        await fetch("http://localhost:1337/api/chairs/" + chair.id, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(obj),
+        });
+        chairDisplay.innerHTML = "";
+        fetchChair();
+      });
     });
   });
 };
